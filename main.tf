964d55a354db210c37a80f892dd38744a58ef74a
Branden <bsmceachern@gmail.com>
2021-12-16 09:36:50 -0600

    created public subnet which will host our EC2 instance for front end

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ resource "aws_vpc" "BKR-VPC" {
         Name = "bkr-vpc"
     }
 }
+
+#subnet-front end (public)
+resource "aws_subnet" "BKR-frontend" {
+    vpc_id = aws_vpc.BKR-VPC.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true"
+
+    tags = {
+        Name = "bkr-frontend"
+    }
+}
+
